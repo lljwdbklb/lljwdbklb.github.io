deff627026dc98f70775a71a4745a058c23241e5
ljj <>
2025-11-20 12:36:33 +0800

    fix

--- assets/config.CnoCz8Wu.js
+++
@@ -1,1 +0,0 @@
-Math.random().toString(36).substr(2,8);const a={normalMin:50,normalMax:200,warningMin:50,warningMax:200},t={targetTime:23,bathTimeStart:20,bathTimeEnd:21,bathDuration:1};export{a as P,t as W};
